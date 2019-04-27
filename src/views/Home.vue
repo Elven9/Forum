0981695e32776d0943131d05c072314d32862498
Elven Lin <elvenlin1998@gmail.com>
2019-04-27 19:38:44 +0800

    feat(): Grid System Completed.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home-container">
-    <div class="home-grid-layout">
+    <div class="home-grid-layout" v-for="b in messages" :key="b.id">
       <MessageBlock
-        v-for="m in messages"
+        v-for="m in b"
         :key="m.id"
         :size="m.size"
         :position="{
-          rowStart: 1,
-          rowEnd: 2,
-          columnStart: 1,
-          columnEnd: 3
+          rowStart: m.position.rowStart,
+          rowEnd: m.position.rowEnd,
+          columnStart: m.position.columnStart,
+          columnEnd: m.position.columnEnd
         }"
         :title="m.title"
         :briefContent="m.subTitle"
@@ -33,16 +33,141 @@ export default {
   components: {
     MessageBlock
   },
-  async mounted() {
-    // 向 DB 拿資料
-    let rawData = await this.$db.collection('articles').where('type', '==', this.$route.path.slice(10)).get();
-    
-    // Construct Message
-    for (let q of rawData.docs) {
-      let payload = { id: q.id };
-      Object.assign(payload, q.data());
-      this.messages.push(payload);
+  methods: {
+    returnMapArea() {
+      if (window.innerWidth > 768) {
+        // 6x6
+        let result = [];
+        for (let i = 0; i < 6; i++) {
+          result.push([]);
+          for (let j = 0; j < 6; j++){
+            result[i][j] = true;
+          }
+        }
+        return result;
+      } else if (window.innerWidth <= 768 && window.innerWidth > 414) {
+        // 6x4
+        let result = [];
+        for (let i = 0; i < 6; i++) {
+          result.push([]);
+          for (let j = 0; j < 4; j++){
+            result[i][j] = true;
+          }
+        }
+        return result;
+      } else if (window.innerWidth <= 414) {
+        // 2x6
+        let result = [];
+        for (let i = 0; i < 6; i++) {
+          result.push([]);
+          for (let j = 0; j < 2; j++){
+            result[i][j] = true;
+          }
+        }
+        return result;
+      }
+    },
+    async getData() {
+      // 向 DB 拿資料
+      let rawData = await this.$db.collection('articles').where('type', '==', this.$route.path.slice(10)).get();
+      
+      // Construct Message
+      let queue = [];
+      for (let q of rawData.docs) {
+        let payload = { id: q.id };
+        Object.assign(payload, q.data());
+        queue.push(payload);
+      }
+
+      // 整理資料
+      let result = [];
+      let map = this.returnMapArea();
+      let mapRowL = map.length;
+      let mapColumnL = map[0].length;
+      let singleBlock = [];
+      
+      for (let i = 0; i < queue.length; i++) {
+        // 掃現在哪些地方還可以 Render
+        let hasPosition = false;
+        for (let r = 0; r < mapRowL; r++) {
+          for (let c = 0; c < mapColumnL; c++) {
+            if (map[r][c]) {
+              // 有空位
+              if (queue[i].size === '1x2') {
+                if (c+1 < mapColumnL && map[r][c+1]) {
+                  queue[i].position = {
+                    rowStart: r+1,
+                    rowEnd: r+2,
+                    columnStart: c+1,
+                    columnEnd: c+3
+                  }
+                  singleBlock.push(queue[i]);
+                  map[r][c] = false;
+                  map[r][c+1] = false;
+                  hasPosition = true;
+                  break;
+                }
+              } else if (queue[i].size === '2x2') {
+                if (r+1 < mapRowL && c+1 < mapColumnL && map[r][c+1] && map[r+1][c] && map[r+1][c+1]) {
+                  queue[i].position = {
+                    rowStart: r+1,
+                    rowEnd: r+3,
+                    columnStart: c+1,
+                    columnEnd: c+3
+                  }
+                  singleBlock.push(queue[i]);
+                  map[r][c] = false;
+                  map[r][c+1] = false;
+                  map[r+1][c] = false;
+                  map[r+1][c+1] = false;
+                  hasPosition = true;
+                  break;
+                }
+              } else if (queue[i].size === '1x1') {
+                queue[i].position = {
+                  rowStart: r+1,
+                  rowEnd: r+2,
+                  columnStart: c+1,
+                  columnEnd: c+2
+                }
+                singleBlock.push(queue[i]);
+                // Update Map
+                map[r][c] = false;
+                hasPosition = true;
+                break;
+              }
+            }
+          }
+          if (hasPosition) break;
+        }
+        if (!hasPosition) {
+          result.push(singleBlock);
+
+          // rst
+          singleBlock = [];
+          map = this.returnMapArea();
+          i--;
+        } else {
+          continue;
+        }
+      }
+      result.push(singleBlock);
+
+      this.messages = result;
+    },
+    clearData() {
+      this.messages = [];
     }
+  },
+  watch: {
+    $route() {
+      this.clearData();
+      this.getData();
+    }
+  },
+  mounted() {
+    this.clearData();
+    this.getData();
   }
 }
 </script>
